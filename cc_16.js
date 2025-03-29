e0b399da7d6ee7e1043a5808c97eb113256971eb
Tiffany Kuo Chang <tiffanyk104@usf.edu>
2025-03-28 20:18:00 -0400

    Task 3: Add fetchProductsAsync using async/await and try/catch

--- cc_16.js
+++ cc_16.js
@@ -1,5 +1,4 @@
 // TASK 2: FETCH PRODUCTS WITH .then()
-
 function fetchProductsThen() {
     
     // Send request to product API
@@ -24,3 +23,22 @@ function fetchProductsThen() {
 }
 
 fetchProductsThen()
+
+// TASK 3: FETCH PRODCUTS WITH async/wait
+async function fetchProductsAsync() {
+    try {
+        const response = await fetch('https://www.course-api.com/javascript-store-products')
+
+        if(!response.ok) {
+            throw new Error('Sorry! Network response not ok.')
+        }
+
+        const products = await response.json()
+
+        // Calling helper function
+        displayProducts(products)
+        
+    } catch (error) {
+        handleError(error); 
+    }  
+}
